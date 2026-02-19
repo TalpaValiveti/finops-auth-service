b2a2cc48b44c63cc2a98bb3bf5088c40322bbd01
Talpa Valiveti <talpa.valiveti@email.com>
2026-02-19 12:34:07 -0600

    Initialize Spring Boot project

---
+++ authservice/src/main/java/com/finops/authservice/FindsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finops.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ authservice/src/test/java/com/finops/authservice/FindsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finops.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindsAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
